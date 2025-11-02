bbb7a702eab4fff3d1ade46b0aa101f1be827841
jakeogh <github.com@v6y.net>
2025-11-01 20:48:10 -0700

    commit showing bug in uncomment_line_in_file() whitespace stripping

--- filetool/__init__.py
+++ filetool/__init__.py
@@ -4,6 +4,7 @@ isort:skip_file
 
 from .filetool import _append_bytes_to_file as _append_bytes_to_file
 from .filetool import comment_out_line_in_file as comment_out_line_in_file
+from .filetool import uncomment_line_in_file as uncomment_line_in_file
 from .filetool import ensure_line_in_config_file as ensure_line_in_config_file
 
 from .append_line_to_file import append_line_to_file as append_line_to_file
